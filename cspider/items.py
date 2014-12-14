813eefc9be685caefd16d58ddf4ef0dee0fcbace
Erik <eschafer90@gmail.com>
2014-12-14 18:07:09 -0500

    Basic functionality
    works to pull content, meta, url.
    
    toDo:
    * add automatic file downloading (pdf, jpg, gif, png, etc??)
    * add a column to track 'depth' of item
    * identify inter-site links, establish tokens or way to regex replace
    them with epa.gov/node/##### when the target page has a none node num

--- cspider/items.py
+++ cspider/items.py
@@ -12,3 +12,9 @@ class CspiderItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     url = scrapy.Field()
+    content = scrapy.Field()
+    keyw = scrapy.Field()
+    descr = scrapy.Field()
+    title = scrapy.Field()
+    ptype = scrapy.Field()
+    subj = scrapy.Field()
--- cspider/spiders/__init__.py
+++ cspider/spiders/__init__.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # This package will contain the spiders of your Scrapy project
 #
 # Please refer to the documentation for information on how to create and manage
@@ -5,6 +6,8 @@
 import scrapy
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors import LinkExtractor
+from scrapy.selector import Selector
+from scrapy.http import HtmlResponse
 from cspider.items import CspiderItem
 
 class MySpider(CrawlSpider):
@@ -13,7 +16,7 @@ class MySpider(CrawlSpider):
     start_urls = ['http://www.epa.gov/cromerr/']
 
     rules = [Rule(LinkExtractor(allow='cromerr'), follow=True, callback='parse_item')]
-    	#Rule(LinkExtractor(allow=(r'cromerr/(\w+/?)+')))]
+        #Rule(LinkExtractor(allow=(r'cromerr/(\w+/?)+')))]
         # Extract links matching 'category.php' (but not matching 'subsection.php')
         # and follow links from them (since no callback means follow=True by default).
         #Rule(LinkExtractor(allow=('category\.php', ), deny=('subsection\.php', ))),
@@ -25,6 +28,17 @@ class MySpider(CrawlSpider):
     def parse_item(self, response):
         item = CspiderItem()
         item['url'] = response.url
+        item['content'] = response.css('div#content').extract()
+        #item['keyw'] = response.css('meta[name="DC.description"]').extract()
+        #item['descr'] = response.css('meta[name="Keyword"]').extract()
+        #item['title'] = response.css('meta[name="DC.title"]').extract()
+        #item['subj'] = response.css('meta[name="DC.Subject.epabrm"]').extract()
+        #item['ptype'] = response.css('meta[name="DC.type"]').extract()
+        item['descr'] = response.xpath('//meta[@name="DC.description"]/@content').extract()
+        item['keyw'] = response.xpath('//meta[@name="Keywords"]/@content').extract()
+        item['title'] = response.xpath('//meta[@name="DC.title"]/@content').extract()
+        item['subj'] = response.xpath('//meta[@name="DC.Subject.epabrm"]/@content').extract()
+        item['ptype'] = response.xpath('//meta[@name="DC.type"]/@content').extract()
         yield item
         #for url in response.xpath('//a/@href').extract():
         #    yield scrapy.Request(url, callback=self.parse)
